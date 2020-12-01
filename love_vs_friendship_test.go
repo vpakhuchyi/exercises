72f060688c12c0ec6be61ae9297afaeaf76b9146
vpakhuchyi <viktor.pakhuchyi@gmail.com>
2020-12-01 17:26:58 +0200

    love vs friendship

--- love_vs_friendship_test.go
+++ love_vs_friendship_test.go
@@ -1,1 +1,41 @@
 package exercises
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_WordsToMarks(t *testing.T) {
+	fmt.Println(WordsToMarks("attitude"))
+}
+
+func WordsToMarks(s string) (result int) {
+	mapping := createMapping()
+	for _, rn := range s {
+		result += mapping[rn]
+	}
+
+	return result
+}
+
+const aIndex = 'a' // index of first eng lowercase character
+const engLettersNumber = 26
+
+func createMapping() map[rune]int {
+	mapping := make(map[rune]int, 26)
+	for c, i := 1, aIndex; i < aIndex+engLettersNumber; i++ {
+		mapping[i] = c
+		c++
+	}
+
+	return mapping
+}
+
+//better solution made by khsadira
+//func WordsToMarks(s string) int {
+//	count := 0
+//	for _, i := range s {
+//		count += int(i) - 'a' + 1
+//	}
+//	return count
+//}
